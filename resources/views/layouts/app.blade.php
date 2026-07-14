7496d3323b72a260b33c56b5480a8e6c98af5fa6
Mohit Palhore <palhoreatul999@.com>
2026-07-14 17:48:23 +0530

    Setup global theme and layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,9 @@
 
     @vite(['resources/css/app.css','resources/js/app.js'])
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
